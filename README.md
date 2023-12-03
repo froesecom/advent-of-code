828d0eeb0d51218545df5a2aa47f36b29334c1dd
Erik F <froesecom@gmail.com>
2023-12-04 08:18:15 +1100

    feat: setup mix project and tests

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Advent of Code
 Where I keep my [Advent of Code](https://adventofcode.com/) scripts
+
+
+## tests
+
+`mix test` to run tests
---
+++ mix.exs
@@ -0,0 +1,10 @@
+defmodule Advent.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :advent,
+      version: "1.0.0"
+    ]
+  end
+end
