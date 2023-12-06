492ba7523f99c22cfa730ea40ee9301b1b01aac3
Erik F <froesecom@gmail.com>
2023-12-06 20:31:10 +1100

    feat: Day 2.1 result

---
+++ lib/2023/2/1.ex
@@ -0,0 +1,30 @@
+defmodule Advent.Day2_1 do
+  defmodule Validator do
+    def run(rule, game) do
+      case rule[:type] do
+        "<=" ->
+          less_or_equal(rule, game)
+      end
+    end
+
+    defp less_or_equal(rule, game) do
+      target = rule[:target]
+      color = rule[:forColor]
+      Enum.all?(game[:checks], fn (check) -> !Map.get(check, color) || Map.get(check, color) |> String.to_integer <= target end)
+    end
+  end
+
+  def run(game_checks, validation_rules) do
+    Enum.reduce(game_checks, 0, fn (game, acc) ->
+      if game_valid?(game, validation_rules) do
+       game[:id] + acc
+      else
+        acc
+      end
+    end)
+  end
+
+  defp game_valid?(game, validation_rules) do
+    Enum.all?(validation_rules, fn rule -> Validator.run(rule, game) end)
+  end
+end
---
+++ test/2023/2/1_test.exs
@@ -0,0 +1,28 @@
+defmodule AdventTest.Day2_1 do
+  use ExUnit.Case
+  doctest Advent.Day2_1
+
+  @validation_rules [
+    %{ type: "<=", forColor: :red, target: 12 },
+    %{ type: "<=", forColor: :green, target: 13 },
+    %{ type: "<=", forColor: :blue, target: 14 },
+
+  ]
+
+  @game_checks [
+    %{id: 1, checks:
+      [%{ red: "1", blue: "3", green: "11" }, %{ blue: "1",red: "5" }, %{ blue: "3", green: "5", red: "13" }, %{ red: "6", blue: "1", green: "4" }, %{ red: "16", green: "12" }]},
+    %{id: 2, checks:
+      [%{ red: "3", blue: "13", green: "5" }, %{ green: "14", blue: "14" }, %{ blue: "9", green: "10",red: "3" }, %{ green: "2", blue: "5" }, %{ green: "11", blue: "3",red: "3" }, %{ blue: "16",red: "2", green: "9" }]},
+    %{id: 3, checks:
+      [%{ blue: "17",red: "5" }, %{ red: "3", green: "11", blue: "17" }, %{ red: "1", blue: "6", green: "9" }, %{ blue: "3", green: "11",red: "1" }, %{ green: "3", red: "10", blue: "11" }, %{ red: "12", green: "3", blue: "15" }]},
+    %{id: 4, checks:
+      [%{ blue: "1",red: "5" }, %{ red: "3", green: "11", blue: "1" }, %{ red: "1", blue: "6", green: "9" }, %{ blue: "3", green: "11",red: "1" }, %{ green: "3", red: "10", blue: "11" }, %{ red: "12", green: "3", blue: "5" }]},
+    %{id: 5,
+      checks: [%{ green: "1", red: "1", blue: "1" }, %{ red: "1", green: "1", blue: "1" }, %{ green: "6", red: "1" }, %{ green: "7" }]} ]
+
+  test "run" do
+    result = Advent.Day2_1.run(@game_checks, @validation_rules)
+    assert result == 9
+  end
+end
