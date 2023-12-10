22193e3bcb05bd2c53c918b460ee9a3e84f7d876
Erik F <froesecom@gmail.com>
2023-12-10 21:42:05 +1100

    feat: Day 7.1 solution

--- lib/2023/7/1.ex
+++ lib/2023/7/1.ex
@@ -3,12 +3,18 @@ defmodule Advent.Day7_1 do
     parse(input)
     |> Enum.map(&prepend_hand_power/1)
     |> Enum.map(&convert_hand_to_base13/1)
+    |> Enum.sort_by(&{Enum.at(&1, 0), Enum.at(&1, 1)}) # sort by hand power then cards in hand
+    |> Enum.with_index(1)
+    |> Enum.reduce(0, fn({hand, index}, acc) ->
+      bid = Enum.at(hand, 2)
+      acc + (bid * index)
+    end)
   end
 
   defp convert_hand_to_base13([power | [hand | bid]]) do
     # stolen from David C.
     # 13 possible cards
-    # By convertig the cards to base 13 value, you can use standard lib to sort
+    # By converting the cards to base 13 value, you can use standard lib to sort
     # -> [power, base13_hand, bid]
     List.flatten([power, Poker.Hand.to_base_13(hand), bid])
   end
@@ -49,7 +55,8 @@ defmodule Poker.Hand do
   }
 
   def to_base_13(hand) do
-    String.codepoints(hand)
+    String.reverse(hand)
+    |> String.codepoints()
     |> Enum.with_index()
     |> Enum.map(fn ({char, index}) ->
       char_atom = String.to_atom(char)
--- test/2023/7/1_test.exs
+++ test/2023/7/1_test.exs
@@ -2,13 +2,8 @@ defmodule AdventTest.Day7_1 do
   use ExUnit.Case
   doctest Advent.Day7_1
 
-  test "parse" do
-    result = Advent.Day7_1.parse(File.read!("test/2023/7/1_input.txt"))
-    assert Enum.at(result, 0) == ["676AA", 840]
+  test "run" do
+    result = Advent.Day7_1.run(File.read!("test/2023/7/1_input.txt"))
+    assert result == 250951660
   end
-
-  # test "run" do
-  #   Advent.Day7_1.run(File.read!("test/2023/7/1_input.txt"))
-  # end
-
 end
