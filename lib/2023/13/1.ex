0c3727335a662c8f2eb3cd469176598c625adaee
Erik F <froesecom@gmail.com>
2023-12-26 09:21:50 +1100

    feat: find row reflections

--- lib/2023/13/1.ex
+++ lib/2023/13/1.ex
@@ -1,13 +1,41 @@
 defmodule Advent.Day13_1 do
   def run(input) do
     parse(input)
+    |> Enum.map(&deduce_row_reflection/1)
   end
 
-  def parse(input) do
-    String.split(input, "\n\n", trim: true)
-    |> Enum.map(fn puzzle ->
-      String.split(puzzle, "\n", trim: true)
-      |> Enum.map(&String.to_charlist/1)
+  defp deduce_row_reflection(puzzle) do
+    indexed_rows = Enum.with_index(puzzle[:input])
+    length_0_indexed = length(indexed_rows) - 1
+
+    Enum.reduce_while(indexed_rows, puzzle, fn {row, index}, acc ->
+      # we're at the end
+      if index == length_0_indexed do
+        {:halt, acc}
+      else
+        case Grid.Reflection.horizontal_at?(indexed_rows, index, index + 1) do
+          true ->
+            {:halt, Map.merge(puzzle, %{reflection: index, reflection_type: :row})}
+
+          false ->
+            {:cont, acc}
+        end
+      end
     end)
+  end
+
+  defp parse(input) do
+    # ~> [{input: puzzle, reflection: nil, reflection_type: nil}]
+    parsed_input =
+      String.split(input, "\n\n", trim: true)
+      |> Enum.map(fn puzzle ->
+        %{
+          input:
+            String.split(puzzle, "\n", trim: true)
+            |> Enum.map(&String.to_charlist/1),
+          reflection: nil,
+          reflection_type: nil
+        }
+      end)
   end
 end
