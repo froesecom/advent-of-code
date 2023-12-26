f6d3ed09252d4b26e81f3ab7e9b9d4f1ca7f6dfd
Erik F <froesecom@gmail.com>
2023-12-26 15:29:47 +1100

    fix: index less than 0 errors

--- lib/2023/13/grid.ex
+++ lib/2023/13/grid.ex
@@ -1,6 +1,9 @@
 defmodule Grid.Reflection do
+  # we've reached the start of the grid, which reflects everything
+  def horizontal_at?(grid, row_index_a, row_index_b) when row_index_a < 0, do: true
+
   def horizontal_at?(grid, row_index_a, row_index_b) do
-    with {row_a, _} when row_a != nil <- Enum.at(grid, row_index_a),
+    with {row_a, _} <- Enum.at(grid, row_index_a),
          {row_b, _} when row_b != nil <- Enum.at(grid, row_index_b) do
       if row_a != row_b do
         # the rows don't match, so not a reflection
@@ -16,12 +19,13 @@ defmodule Grid.Reflection do
     end
   end
 
-  def vertical_at?(grid, column_index_a, column_index_b) do
-    Enum.reduce_while(grid, true, fn {row, _i}, _acc ->
-      codepoints = String.codepoints(row)
+  # we've reached the start of the grid, which reflects everything
+  def vertical_at?(grid, column_index_a, column_index_b) when column_index_a < 0, do: true
 
-      with column_a when column_a != nil <- Enum.at(codepoints, column_index_a),
-           column_b when column_b != nil <- Enum.at(codepoints, column_index_b) do
+  def vertical_at?(grid, column_index_a, column_index_b) do
+    Enum.reduce_while(grid, true, fn row, _acc ->
+      with column_a <- Enum.at(row, column_index_a),
+           column_b when column_b != nil <- Enum.at(row, column_index_b) do
         if column_a != column_b do
           # the columns don't match, so not a reflection
           {:halt, false}
